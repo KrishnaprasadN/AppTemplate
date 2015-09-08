98ddf3d61725ce90010b24581ff7ab56dd8cd2a5
Krishnaprasad N <krishnaprasadnaravi@gmail.com>
2015-09-08 12:38:17 +0530

    Added function to check if app is in background

--- app/src/main/java/com/app/template/utils/CommonUtils.java
+++ app/src/main/java/com/app/template/utils/CommonUtils.java
@@ -1,5 +1,7 @@
 package com.app.template.utils;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -124,5 +127,26 @@ public class CommonUtils {
         return Environment.getExternalStorageState().equals(
                 Environment.MEDIA_MOUNTED);
 
+    }
+
+    /**
+     * whether application is in background
+     * <ul>
+     * <li>need use permission android.permission.GET_TASKS in Manifest.xml</li>
+     * </ul>
+     *
+     * @param context
+     * @return if application is in background return true, otherwise return false
+     */
+    public static boolean isApplicationInBackground(Context context) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> taskList = am.getRunningTasks(1);
+        if (taskList != null && !taskList.isEmpty()) {
+            ComponentName topActivity = taskList.get(0).topActivity;
+            if (topActivity != null && !topActivity.getPackageName().equals(context.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
